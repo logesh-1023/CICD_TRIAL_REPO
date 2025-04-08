da008677093594fd77cb817a62a7aacb7a3eaa4f
Selvaraj <lselva859@apac.comcast.com>
2025-04-08 16:09:33 +0530

    lets go 8

--- tasks/model.py
+++ tasks/model.py
@@ -1,6 +1,6 @@
 import sys
 def main():
-    print("Hey all set!.Ready for Production , lets goooooooo!!!!. TESTING 7 ", sys.argv[1:])
+    print("Hey all set!.Ready for Production , lets goooooooo!!!!. TESTING 8 ", sys.argv[1:])
 
 if __name__ == "__main__":
     main()
